59cee62528817ba8c2f84b898e14a31029af128f
rveldman <rveldman@pointlogic.com>
2019-08-17 23:31:11 +0200

    deply

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -4,7 +4,7 @@ set -e
 
 docker build -t ${GCLOUD_HOSTNAME}/${GCLOUD_PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 echo "after docker"
-$GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 echo "after auth"
 gcloud --quiet config set project ${GCLOUD_PROJECT_ID}
