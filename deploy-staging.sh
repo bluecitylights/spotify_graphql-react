2e25107db61629981a29a8db36d4c926b061cfc3
rveldman <rveldman@pointlogic.com>
2019-08-17 23:20:54 +0200

    gcloud auth

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo "before docker"
 docker build -t ${GCLOUD_HOSTNAME}/${GCLOUD_PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 echo "after docker"
+$GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-echo "after auth docker"
-# gcloud --quiet config set project $PROJECT_NAME_STG
+echo "after auth"
+gcloud --quiet config set project ${GCLOUD_PROJECT_ID}
 # gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
-# gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet config set compute/zone ${GCLOUD_COMPUTE_ZONE}
 # gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
 gcloud docker push ${GCLOUD_HOSTNAME}/${GCLOUD_PROJECT_ID}/${DOCKER_IMAGE_NAME}
