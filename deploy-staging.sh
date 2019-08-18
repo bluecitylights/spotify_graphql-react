d259b5e424a5c30e055a146eeb901aaa37c6987b
rveldman <rveldman@pointlogic.com>
2019-08-18 17:55:36 +0200

    fix

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-
-set -e
-
 docker build -t $GCLOUD_HOSTNAME/$GCLOUD_PROJECT_ID/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT .
-
-echo "after docker"
-echo -e $GCLOUD_SERVICE_KEY | base64 --decode -i > $HOME/gcloud-service-key.json
-gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
-echo "after auth"
-gcloud --quiet config set project ${GCLOUD_PROJECT_ID}
-# gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
+echo -e $GCLOUD_SERVICE_KEY | base64 --decode -i > $HOME/gcloud-credentials.json
+gcloud auth activate-service-account --key-file $HOME/gcloud-credentials.json
+gcloud config set project $GCLOUD_PROJECT_ID
 gcloud --quiet config set compute/zone $GCLOUD_COMPUTE_ZONE
-# gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
-
 gcloud docker push $GCLOUD_HOSTNAME/$GCLOUD_PROJECT_ID/$DOCKER_IMAGE_NAME
-echo "after push"
