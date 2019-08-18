067844d0464e238136098fd27695c8b2e91a0966
rveldman <rveldman@pointlogic.com>
2019-08-18 21:10:41 +0200

    ssh-keygen

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -4,5 +4,5 @@ echo -e $GCLOUD_SERVICE_KEY | base64 --decode -i > $HOME/gcloud-credentials.json
 gcloud auth activate-service-account --key-file $HOME/gcloud-credentials.json
 gcloud config set project $GCLOUD_PROJECT_ID
 gcloud --quiet config set compute/zone $GCLOUD_COMPUTE_ZONE
-cat $HOME/gcloud-credentials.json | docker login -u _json_key $GCLOUD_HOSTNAME
+ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 docker push $GCLOUD_HOSTNAME/$GCLOUD_PROJECT_ID/$DOCKER_IMAGE_NAME
