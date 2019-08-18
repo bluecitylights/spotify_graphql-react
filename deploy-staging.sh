a19de47245c02cad1b7c735d4db9e7c0e94cc732
rveldman <rveldman@pointlogic.com>
2019-08-18 18:00:52 +0200

    fix docker push

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -4,4 +4,4 @@ echo -e $GCLOUD_SERVICE_KEY | base64 --decode -i > $HOME/gcloud-credentials.json
 gcloud auth activate-service-account --key-file $HOME/gcloud-credentials.json
 gcloud config set project $GCLOUD_PROJECT_ID
 gcloud --quiet config set compute/zone $GCLOUD_COMPUTE_ZONE
-gcloud docker push $GCLOUD_HOSTNAME/$GCLOUD_PROJECT_ID/$DOCKER_IMAGE_NAME
+docker push $GCLOUD_HOSTNAME/$GCLOUD_PROJECT_ID/$DOCKER_IMAGE_NAME
