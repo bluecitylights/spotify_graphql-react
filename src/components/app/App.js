fc4cba00c68fb55f6dce8a29d64fc4fc115cee0f
rveldman <rveldman@pointlogic.com>
2019-09-06 08:53:41 +0200

    added todo about localstorage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -32,7 +32,7 @@ import { setContext } from 'apollo-link-context';
 const client = new ApolloClient({
   uri: process.env.REACT_APP_SPOTIFY_ANALYZER_API,
   request: operation => {
-    const user = JSON.parse(window.localStorage.getItem('sp_user'))
+    const user = JSON.parse(window.localStorage.getItem('sp_user')) // todo: get from auth context instead of localstorage
     operation.setContext({
       headers: {
         Authorization: user ? `Bearer ${user.token}` : ""
