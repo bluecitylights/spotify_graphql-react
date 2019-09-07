9352634676c815daa3225671c9de9256dd36eb16
rveldman <rveldman@pointlogic.com>
2019-09-07 20:21:33 +0200

    added login, logout, loading and error components

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,8 @@ import {Artist, Artists} from '../src/components/Artists'
 
 import {User, NoUser, Login, Logout} from '../src/components/Layout/NavHeaderEx'
 
+import {Loading, Error} from '../src/components/utils'
+
 storiesOf('User', module)
     .add('without image', () => (
         <User collapsed = {true}  user = {{display_name: "Richard"}} />
@@ -16,6 +18,18 @@ storiesOf('NoUser', module)
     .add('no user', () => (
         <NoUser />
     ))
+
+storiesOf('Loading', module)
+    .add('loading', () => (<Loading/>))
+
+storiesOf('Error', module)
+    .add('error', () => (<Error/>))    
+
+storiesOf('Login', module)
+    .add('login', () => (<Login/>))
+
+storiesOf('Logout', module)
+    .add('logout', () => (<Logout/>))    
 
 storiesOf('Artist', module)
     .add('artist', () => (
