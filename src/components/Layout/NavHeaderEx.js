f2bdbade7d2edf663d5897b7305278988c71036c
rveldman <rveldman@pointlogic.com>
2019-09-07 12:37:21 +0200

    user compoonent should get user as prop

--- src/components/Layout/NavHeaderEx.js
+++ src/components/Layout/NavHeaderEx.js
@@ -19,7 +19,7 @@ const Login = () => {
   return (
   <div className='App'>
       {/* <Button onClick={() => auth.signin()}>Signin</Button> */}
-      <a href={`${process.env.REACT_APP_SPOTIFY_ANALYZER_LOGIN}?client_redirect_uri=${process.env.REACT_APP_SPOTIFY_AUTH_CALLBACK}`}>Login</a>
+      <a href={`${process.env.REACT_APP_SPOTIFY_ANALYZER_LOGIN}?client_redirect_uri=${process.env.REACT_APP_SPOTIFY_AUTH_CALLBACK}`}>Sign in</a>
       {/* <a href="/login" dangerouslySetInnerHTML={{__html: loginSVG}}></a> */}
   </div>
 )}
@@ -28,17 +28,18 @@ const Logout = () => {
   const auth = useAuth();
   return (
   <div className='App'>
-      <Button onClick={() => auth.signout()}>Signout</Button>
+      <Button onClick={() => auth.signout()}>Sign out</Button>
   </div>
 )}
 
 
-const User = ({collapsed}) => {
-  const auth = useAuth()
+const User = ({user, collapsed}) => {
   return(
   <React.Fragment>
      
       <Avatar
+        src = {user.image}
+        alt = {user.display_name}
         style={{
           width: collapsed ? 48 : 60,
           height: collapsed ? 48 : 60,
@@ -47,11 +48,11 @@ const User = ({collapsed}) => {
       />
       <div style={{ paddingBottom: 16 }} />
       <Typography variant={"h6"} noWrap>
-        {auth.user.token}
+        {user.display_name}
       </Typography>
-      <Typography color={"textSecondary"} noWrap gutterBottom>
-        {auth.user.email}
-      </Typography>
+      {/* <Typography color={"textSecondary"} noWrap gutterBottom>
+        {user.display_name}
+      </Typography> */}
       <Logout />
     </React.Fragment>
 )}
@@ -60,7 +61,7 @@ const NavHeaderEx = ({ collapsed }) => {
   const auth = useAuth();
   return (<>
     <div style={{ padding: collapsed ? 8 : 16, transition: "0.3s" }}>
-    {auth.user ? (<User collapsed = {collapsed} />) : (<Login />)}
+    {auth.user ? (<User user = {{display_name: "Richard", image: "https://profile-images.scdn.co/images/userprofile/default/8a3ebfc36a61a477ff1dacb4d1aa370c6fe02ce6"}} collapsed = {collapsed} />) : (<Login />)}
     </div>
     <Divider />
   </>)
